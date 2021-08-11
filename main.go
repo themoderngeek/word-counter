705d36667fd368f10c62e8ea5955c4b91f338b9e
Mark Taylor <moderngeekish@gmail.com>
2021-08-11 17:15:22 +0100

    added flags to the tool

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	linesFlag := flag.Bool("l", false, "Count Lines")
+	bytesFlag := flag.Bool("b", false, "Count Bytes")
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *linesFlag, *bytesFlag))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
+
 	wc := 0
 	for scanner.Scan() {
 		wc++
